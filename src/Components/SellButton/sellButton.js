df674bad9a9a16fbcf5fa4eec36608064230cd3a
Edwin Aragon <ared2728@gmail.com>
2020-10-10 12:28:06 -0600

    Add SellButton component

---
+++ src/Components/SellButton/sellButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SellButton extends React.Component{
+    render(){
+        return(
+            <button>Sell</button>
+        );
+    }
+}
+
+export default SellButton;
